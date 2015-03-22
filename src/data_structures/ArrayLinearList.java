f569e321322b03546a423face793e46906049b99
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-22 13:55:01 -0700

    Update ArrayLinearList.java
    binary search delete

--- src/data_structures/ArrayLinearList.java
+++ src/data_structures/ArrayLinearList.java
@@ -1,183 +1,227 @@
- /*
- *Alicia Guerra
- *Professor Steve Price
- *Computer Science 310
- *masc1529
- */
-package data_structures;
+/*
+**Alicia Guerra
+**Professor Steve Price
+**CS 310: Data Structures
+**masc 0879
+*/
 
+package data_structures;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class ArrayLinearList<E> implements LinearListADT<E> {
-
     private int currentSize, maxSize;
     private E[] storage;
 
     public ArrayLinearList() {
-        currentSize = 0;
-        maxSize = 1000;
-        storage = (E[]) new Object[maxSize];
+    currentSize=0;
+    maxSize=DEFAULT_MAX_CAPACITY;
+    storage=(E[]) new Object[maxSize];
     }
 
-    public void insert(E obj) {
-        checkSize();
-        /*checkSize after the iterator in Step 12.*/
-       storage[currentSize++]=obj;
+/*Adds the object obj to the end of the list.*/   
+    public void addLast(E obj) {
+       growStorage();
+       storage[currentSize+1]= obj;
+       currentSize++;
     }
-    /*2. Removes the object located at the paramater index position (zero-based).*/
-    /*3. Throws IndexOutofBoundsException if the index does not map to a valid
-     position within the list.*/
 
-    public E remove(int location) {
-        return shiftDelete(location);
+/*Adds the Object obj to the beginning of the list.*/
+    @Override
+    public void addFirst(E obj) {
+            growStorage();
+            int location=1;
+/*We shift everything to the right of the insertion point.*/
+        for(int i=currentSize;i>location;i--){
+            storage[i+1]=storage[i];
+            }       
+        storage[location]=obj;
+/*We're adding elements so currentSize is increasing.*/
+        currentSize++;
     }
-    /*4. Removes and returns the parameter object obj from the list if the list
-     contains it, null otherwise.*/
 
-    public E remove(E obj) {
-        int check = binSearchFindObjectIndex(obj, 0, currentSize - 1);
-        return shiftDelete(check);
+    public void growStorage(){
+        if(currentSize>=maxSize){
+        setMaxSize(maxSize*2);
     }
-    /*5. Returns the parameter object located at the parameter index position
-     (zero-based).*/
-
-    public E get(int location) {
-        E check;
-        try {
-            check = storage[location];
-        } 
-    /*6. Throws IndexOutofBoundsException if the index does not map to a valid 
-    position within the underlying array.*/ 
-        catch (IndexOutOfBoundsException E) {
-            check = null;
+        else if(currentSize==maxSize/4)
+        {
+        setMaxSize(maxSize/2);
         }
-        return check;
+       E[] tempStorage = (E[]) new Object[maxSize];
+       for(int i=1;i<=currentSize;i++){
+           tempStorage[i]=storage[i];
+       }
+       storage=tempStorage;
+    }   
+    private void setMaxSize(int size){
+        maxSize=size;
+    }  
+    
+
+/*Inserts the Object obj at the position indicated. If there is an element at that
+position, all elements from that location to the end of the list are shifted down
+to make room for that new insertion. The location is one-based. If the 
+location>size()+1, then a RunTime Exception is thrown. List elements must be
+contiguous*/   
+    @Override
+    public void insert(E obj, int location) {
+        growStorage();
+/*If the location is greater than the currentSize, a runtime exception is thrown.*/        
+        if(location>currentSize||location<1){
+        throw new RuntimeException("Index Out Of Bounds");    
+        }
+/*If there is an element at that position, move all elements from that location to
+the end of the list to make room for that new insertion.*/
+        for(int i=currentSize;i>location;i--){
+            storage[i+1]=storage[i];
+        }
+        storage[location]=obj;
+/*We performing an insertion, so the currentSize increases.*/
+        currentSize++;
     }
-    /*7. Returns the parameter object obj if the list contains it, null otherwise.*/
+    
+/*Removes the object at the parameter location (one-based) Throws a runtime
+exception if the location does not map to a valid position within the list.*/
+    @Override
+    public E remove(int location) {
+/*Throws a runtime exception if the location does not map to a valid position
+within the list.*/
+    if(location>currentSize || location<1){
+        throw new RuntimeException("Index Out of Bounds");
+        }
+     E tempObject=storage[location];
+/*We shift everything to the right of the index.*/
+     for(int i=location;i>=currentSize;i--){
+         storage[i+1]=storage[i];
+     }
+/*The size is decreasing since we're removing.*/
+     currentSize--;
+     return tempObject;
+    }
 
-    public E get(E obj) {
-        int check = binSearchFindObjectIndex(obj, 0, currentSize - 1);
-        return storage[check];}
-    /*8. Returns true if the parameter object obj is in the list, false otherwise.*/
-    /*Just like the code in Riggins' SlowContainer data structure.*/
+/*Removes and returns the parameter object obj from the list if the list contains
+it, null otherwise. The ordering of the list is preserved. The list may contain
+deuplicate elements. This method removes and returns the first matching element
+found when removing the list from the first position.*/
+    @Override
+    public E remove(E obj) {
+      int location=locate(obj); 
+      if(location==-1){
+          return null;
+      }
+      /*Shifts the elements to the left of the list.*/
+      for(int i=location;i<=currentSize;i++){
+          storage[i]=storage[i+1];
+      }
+      E tempObject=storage[location];
+      growStorage();
+      currentSize--;
+      return tempObject;
+      }
 
+/*Removes and returns the parameter object obj in the first position in the list
+if the list is not empty, null if the list is empty. The ordering of the list is
+preserved.*/
+    @Override
+    public E removeFirst() {
+    growStorage();
+    int location=1;
+    E tempObject=storage[location];
+/*Move the objects in the array to the right.*/
+    for(int i=currentSize;i>1;i--){
+       storage[i+1]=storage[i];
+       }
+/*We're removing things so currentSize is decreasing.*/
+       currentSize--;
+    return tempObject;
+    }
+
+/*Removes and returns the parameter object obj in the last position in the list if
+the list is not empty, null if not empty. The order of the list is preserved.*/    
+    @Override
+    public E removeLast() {
+    growStorage();
+    E tempObject=storage[currentSize];
+/*We're removing things so currentSize is decreasing.*/
+    currentSize--;
+    return tempObject;
+    }
+
+/*Returns the parameter object located at the parameter location position (one
+based). Throws a runtime exception if the location does not map to a valid position
+within the list.*/
+    @Override
+    public E get(int location) {
+/*Throws a runtime exception if the location does not map to a valid position
+within the list.*/
+        if(location>storage.length|| location<1){
+      throw new RuntimeException("Index Out of Bounds");
+              }
+/*Returns the parameter object located at the parameter location position.*/
+      return storage[location];
+    }
+
+/**Returns true if the parameter object is in the list, false otherwise.*/    
+    @Override
     public boolean contains(E obj) {
-        for (int i = 0; i < currentSize; i++) 
+        for(int i=1;i<=currentSize;i++)
+        //for (int i = 0; i <=currentSize-1; i++)
             if (((Comparable<E>) obj).compareTo(storage[i]) == 0)
                 return true;
-                return false;
+                return false;         
     }
-    /*9. The list is returned to an empty state.*/
 
-    public void clear() {
-        setMaxSize(100);
-        storage = (E[]) new Object[maxSize];
+/*Returns the one-based location of the parameter object obj is in the list,
+false otherwise.*/    
+    @Override
+    public int locate(E obj) {
+        //for(int i=0;i<=currentSize;i++){
+        for(int i=1;i<=currentSize;i++){
+            if(this.storage[i].equals(obj))
+                return i;
+        }
+         return -1;
     }
-    /*10. Returns true if the list is empty, otherwise false.*/
-    /*Just like the code in Riggins' SlowContainer data structure.*/
 
+ /**The list is returned to an empty state.*/
+    @Override
+    public void clear() { 
+       currentSize=0;
+    }
+
+/*Returns true if the list is empty, otherwise false.*/    
+    @Override
     public boolean isEmpty() {
-        return currentSize == 0;
+        return currentSize==0;
     }
-    /*11. Returns the number of objects currently in the list*/
-    /*Just like the code in Riggins' SlowContainer data structure*/
 
+/*Returns the number of objects currently in the list.*/    
+    @Override
     public int size() {
         return currentSize;
     }
-    /*12. Returns an iterator of the values in the list, presented in the same
-     order as the list.*/
-    /*This part of the code was copied from Riggins' SlowContainer program.*/
 
+/*Returns an iterator of the values in the list, presented in the same order
+as the underlying order of the list.*/    
+    @Override
     public Iterator<E> iterator() {
         return new IteratorHelper();
     }
-
-    class IteratorHelper implements Iterator<E> {
-
+ class IteratorHelper implements Iterator<E> {
         int iterIndex;
-
         public IteratorHelper() {
-            iterIndex = 0;
+            iterIndex=1;
         }
-
         public boolean hasNext() {
             return iterIndex < currentSize;
         }
-
         public E next() {
             if (!hasNext()) {
                 throw new NoSuchElementException();
             }
             return storage[iterIndex++];
         }
-
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
-    }
-    /*checkSize from part 1*/
-    
-    public void checkSize() {
-        if (currentSize == maxSize) {
-            setMaxSize(maxSize * 2);
-        } else if (currentSize <= maxSize / 4) {
-            setMaxSize(maxSize / 2);
-        }
-        E[] tempStorage = (E[]) new Object[maxSize];
-        for (int i = 0; i < currentSize; i++) {
-            tempStorage[i] = storage[i];
-        }
-        storage = tempStorage;
-    }
-
-    private void setMaxSize(int size) {
-        maxSize = size;
-    }
-
-    private E shiftDelete(int dIndex) {
-        checkSize();
-        E check;
-        try {
-            check = storage[dIndex];
-        } catch (IndexOutOfBoundsException e) {
-            check = null;
-        }
-        for (int i = currentSize - 1; dIndex <= i; dIndex++) {
-            storage[dIndex] = storage[dIndex + 1];
-        }
-        currentSize--;
-        return check;
-    }
-    /*Binary Search Method*/
-    private int binSearchFindObjectIndex(E obj, int low, int high) {
-        if (low > high) {
-            return -1;
-        }
-        int mid = (low + high) / 2, comp = 1;
-        if (comp == 0) {
-            return mid;
-        } else if (comp < 0) {
-            return binSearchFindObjectIndex(obj, low, mid - 1);
-        }
-        return binSearchFindObjectIndex(obj, mid + 1, high);
-    }
-
-    private int binSearchFindInsertionPoint(E obj, int low, int high) {
-        if (obj == null) {
-            return 0;
-        }
-        if (low > high) {
-            return low;
-        }
-        int mid = (low + high) / 2, comp = 1;
-        if (comp == 0) {
-            return mid;
-        } else if (comp < 0) {
-            return binSearchFindInsertionPoint(obj, low, mid - 1);
-        }
-        return binSearchFindInsertionPoint(obj, mid + 1, high);
-    }
+    }   
 }
    
